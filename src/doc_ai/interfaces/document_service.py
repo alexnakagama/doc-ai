f8f02e47a9e11e8381066e1f8800ba121744a4f1
alexnakagama <alexnakagama4@gmail.com>
2026-09-26 12:21:30 -0300

    fixed the interface

--- src/doc_ai/interfaces/document_service.py
+++ src/doc_ai/interfaces/document_service.py
@@ -4,7 +4,4 @@ from doc_ai.models.document import Document
 
 
 class DocumentServiceInterface(Protocol):
-    def __init__(self):
-        self.documents: list[Document] = []
-
     async def get_documents(self) -> list[Document]: ...
